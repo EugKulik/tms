f75ef1c5cfc6740292cc3200ea52b3af7dcf43e8
EKulik <nobilis92@gmail.com>
2019-10-08 23:10:28 +0300

    task6 done

--- src/homework_2/task_6/Task6.java
+++ src/homework_2/task_6/Task6.java
@@ -1,7 +1,9 @@
 package homework_2.task_6;
 
 public class Task6 {
-    public static void main(String[] args) {
+    public static void main(
+
+            String[] args) {
         for (int i = 2; i <= 100; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
