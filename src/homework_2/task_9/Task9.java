4dbfa7a70970704537035352c3a4bfec137a1ba0
EKulik <nobilis92@gmail.com>
2019-10-09 12:05:28 +0300

    task9 done

--- src/homework_2/task_9/Task9.java
+++ src/homework_2/task_9/Task9.java
@@ -2,9 +2,27 @@ package homework_2.task_9;
 
 public class Task9 {
     public static void main(String[] args) {
-        String s = "*";
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 0; j < 4-i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j > 0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
+        System.out.println(" ");
 
+        for (int i = 4; i >= 0; i--) {
+            for (int j = 0; j < 4-i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j > 0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
     }
 }
