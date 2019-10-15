bffc0cef94065e77945f2267ace8302c299590a1
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-15 20:47:09 +0300

    Update Task3.java

--- src/homework_3/task_3/Task3.java
+++ src/homework_3/task_3/Task3.java
@@ -2,8 +2,10 @@ package homework_3.task_3;
 
 import java.util.Arrays;
 
+// форматируй код
 public class Task3 {
     public static void main(String[] args) {
+        // int [] - пробел после int не нужен
         int [] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         for (int i = 0; i <array.length ; i++) {
