12f0e0f6d321f0c04e0585c9d93b1aea137834f4
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-30 22:31:05 +0300

    Update Task9Main.java

--- src/homework_7/task_9/Task9Main.java
+++ src/homework_7/task_9/Task9Main.java
@@ -1,6 +1,7 @@
 package homework_7.task_9;
 
 /*Есть массив строк, вывести на экран все строки, длина которых больше 5 символов*/
+// +
 public class Task9Main {
     public static void main(String[] args) {
         int size = 5;
