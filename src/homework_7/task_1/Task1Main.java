1cdf5d7a5a5b397bb9519de25f6bbd74590b2bbc
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-02 10:07:57 +0300

    Update Task1Main.java

--- src/homework_7/task_1/Task1Main.java
+++ src/homework_7/task_1/Task1Main.java
@@ -1,6 +1,8 @@
 package homework_7.task_1;
 
 //Игра в слова. Есть строка “engineering”. Нужно выделить подстроку так, чтобы получилось два слова “engine” и “ring”.
+
+//+
 public class Task1Main {
     public static void main(String[] args) {
         String string = "engineering";
