b01d4b36cfaec69b7b5bb92b0c6b46d98ef388a2
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-30 12:15:15 +0300

    Update Task2Main.java

--- src/homework_15/task_2/Task2Main.java
+++ src/homework_15/task_2/Task2Main.java
@@ -11,6 +11,7 @@ import java.util.List;
 /*Есть студенты. Каждый студент записан в отдельном .json файле.
 У каждого студента есть номер группы, имя, фамилия, факультет и список любимых предметов для изучения.
 Прочитать студентов при помощи Jackson JSON парсера и добавить их в какой-нибудь лист.*/
+
 public class Task2Main {
 
     public static void main(String[] args) throws IOException {
@@ -26,7 +27,7 @@ public class Task2Main {
         File[] folderEntries = folder.listFiles();
         for (File entry : folderEntries) {
             if (entry.isFile()) {
-                String entryName = entry.toString();
+                String entryName = entry.toString(); // file.getAbsolutePath, не стоит использовать toString для таких целей
                 Student student = mapper.readValue(new File(entryName), Student.class);
                 students.add(student);
             }
